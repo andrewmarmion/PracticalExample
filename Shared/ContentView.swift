96b2c514756b6367c70b5ed14c8fa75bc073e30d
Andrew Marmion <andrew.marmion@gmail.com>
2021-10-09 13:29:17 +0100

    Add comments and refactor list item fro list

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -15,7 +15,7 @@ enum ContentType: String, Decodable {
 
 struct ItemAttributes: Decodable {
     let name: String
-    let description: String
+    let description: String // is html in the videos, do we need to handle that?
     let content_type: ContentType
     let card_artwork_url: URL
     let released_at: Date
@@ -45,13 +45,12 @@ class ViewModel: ObservableObject {
 
     private var articles: [FeedItems.FeedItem] = []
     private var videos: [FeedItems.FeedItem] = []
+    private var both: [FeedItems.FeedItem] = []
+
     private var cancellables = Set<AnyCancellable>()
 
-    init() {
-        load()
-    }
-
     func load() {
+        self.state = .loading
         let articlesURL = URL(string: "https://raw.githubusercontent.com/raywenderlich/ios-interview/master/Practical%20Example/articles.json")!
 
         let videosURL = URL(string: "https://raw.githubusercontent.com/raywenderlich/ios-interview/master/Practical%20Example/videos.json")!
@@ -61,16 +60,23 @@ class ViewModel: ObservableObject {
 
         articlePublisher
             .combineLatest(videoPublisher)
+//            .delay(for: 2, scheduler: DispatchQueue.main)
             .receive(on: DispatchQueue.main)
             .sink { completion in
                 if case let .failure(error) = completion {
                     self.state = .error(error)
                 }
             } receiveValue: { [weak self] articles, videos in
-                self?.articles = articles.dateSorted()
-                self?.videos = videos.dateSorted()
-                self?.selectedList = .all
-                self?.state = .loaded
+                guard let self = self else { return }
+
+                // make sure the articles and videos are sorted so we only have to show perform the sort once
+                self.articles = articles.dateSorted()
+                self.videos = videos.dateSorted()
+                self.both = (articles + videos).dateSorted()
+                self.state = .loaded
+
+                // reset the selectedList so that we can refresh what is shown on screen
+                self.selectedList = self.selectedList
             }
             .store(in: &cancellables)
 
@@ -82,22 +88,23 @@ class ViewModel: ObservableObject {
     }
 
     private func publisher(for url: URL) -> AnyPublisher<[FeedItems.FeedItem], Error> {
+        // We should be injeting this so that we can use any type of dataTaskPublisher
         URLSession.shared.dataTaskPublisher(for: url)
             .tryMap { $0.data }
-            .decode(type: FeedItems.self, decoder: decoder())
+            .decode(type: FeedItems.self, decoder: feedItemDecoder())
             .map { $0.data }
             .eraseToAnyPublisher()
     }
 
     private func handle(selectedList: SelectedList) {
         switch selectedList {
-        case .all: self.items = (self.articles + self.videos).dateSorted()
-        case .articles: self.items = self.articles
-        case .videos: self.items = self.videos
+        case .all: items = both
+        case .articles: items = articles
+        case .videos: items = videos
         }
     }
 
-    private func decoder() -> JSONDecoder {
+    private func feedItemDecoder() -> JSONDecoder {
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .formatted(DateFormatter.customDate)
         return decoder
@@ -105,6 +112,7 @@ class ViewModel: ObservableObject {
 }
 
 extension DateFormatter {
+    // We are not displaying the date and we only need it for sorting purposes
     static let customDate: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
@@ -136,6 +144,7 @@ struct ContentView: View {
                 switch viewModel.state {
                 case .error(let error):
                     Text("Loading error \(error.localizedDescription)")
+                        .padding()
 
                 case .loaded:
 
@@ -151,10 +160,23 @@ struct ContentView: View {
                     }
 
                 case .loading:
-                    Text("Loading")
+                    ProgressView("Loading...")
+                        .font(.title)
                 }
             }
             .navigationTitle("Courses")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        viewModel.load()
+                    } label: {
+                        Image(systemName: "arrow.counterclockwise")
+                    }
+                }
+            }
+            .onAppear {
+                viewModel.load()
+            }
         }
     }
 }
@@ -165,25 +187,33 @@ struct ListView: View {
     var body: some View {
         List {
             ForEach(items) { item in
-                VStack(alignment: .leading) {
-                    HStack(alignment: .top) {
-                        ItemImage(url: item.attributes.card_artwork_url)
-                            .clipShape(RoundedRectangle(cornerRadius: 8))
-                            .frame(width: 100, height: 100)
-
-                        VStack(alignment: .leading, spacing: 10) {
-                            Text(item.attributes.name)
-                                .fixedSize(horizontal: false, vertical: true)
-                            Image(systemName: getImageName(for: item.attributes.content_type))
-                        }
-
-                        Spacer()
-                    }
-
-                    Text(item.attributes.description)
-                        .fixedSize(horizontal: false, vertical: true)
-                }
+                ListItem(item: item)
             }
+        }
+    }
+}
+
+struct ListItem: View {
+
+    let item: FeedItems.FeedItem
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                ItemImage(url: item.attributes.card_artwork_url)
+
+                VStack(alignment: .leading, spacing: 10) {
+                    Text(item.attributes.name)
+                        .bold()
+                        .fixedSize(horizontal: false, vertical: true)
+                    Image(systemName: getImageName(for: item.attributes.content_type))
+                }
+
+                Spacer()
+            }
+
+            Text(item.attributes.description)
+                .fixedSize(horizontal: false, vertical: true)
         }
     }
 
@@ -201,20 +231,18 @@ struct ItemImage: View {
     let url: URL
     var body: some View {
         // TODO: AsyncImage is not available in macOS 11.3 so we need to roll our own version of it
-        if #available(iOS 15.0, *) {
-            AsyncImage(url: url) { image in
-                image
-                    .resizable()
-                    .scaledToFit()
-            } placeholder: {
-                ZStack {
-                    ProgressView()
-                    Color.gray.opacity(0.1)
-                }
+        AsyncImage(url: url) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            ZStack {
+                ProgressView()
+                Color.gray.opacity(0.1)
             }
-        } else {
-            Image(systemName: "xmark.octagon.fill")
         }
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+        .frame(width: 100, height: 100)
     }
 }
 
