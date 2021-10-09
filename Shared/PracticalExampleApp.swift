e1d58427484ae67a2abef7957d5073e3eaa846fd
Andrew Marmion <andrew.marmion@gmail.com>
2021-10-09 10:15:52 +0100

    Initial Commit

---
+++ Shared/PracticalExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticalExampleApp.swift
+//  Shared
+//
+//  Created by Andrew Marmion on 09/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PracticalExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
