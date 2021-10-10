44cadfe0c30c3d670d1ae0e223aff34086d89eec
Andrew Marmion <andrew.marmion@gmail.com>
2021-10-10 16:59:18 +0100

    Refactor publishesEmptyResponse to be publishesSuccessResponse(articles:videos:) with default values

--- PracticalExampleTests/ViewModelTests.swift
+++ PracticalExampleTests/ViewModelTests.swift
@@ -5,7 +5,7 @@ import XCTest
 final class ViewModelTests: XCTestCase {
 
     func test_init_initialState() {
-        let feedLoader = StubbedFeedLoader(stubbedResponse: StubbedFeedLoader.publishesEmptyResponse())
+        let feedLoader = StubbedFeedLoader(stubbedResponse: StubbedFeedLoader.publishesSuccessResponse())
         let sut = ViewModel(feedLoader: feedLoader)
 
         XCTAssertEqual(sut.state, .empty)
@@ -13,8 +13,8 @@ final class ViewModelTests: XCTestCase {
         XCTAssertEqual(sut.selectedList, .all)
     }
 
-    func test_load_errorInFeedLoaderDisplaysError() {
-        let feedLoader = StubbedFeedLoader(stubbedResponse: StubbedFeedLoader.publlishesErrorResponse())
+    func test_load_errorInFeedLoaderCreatesErrorState() {
+        let feedLoader = StubbedFeedLoader(stubbedResponse: StubbedFeedLoader.publishesErrorResponse())
         let sut = ViewModel(feedLoader: feedLoader)
 
         sut.load()
@@ -38,13 +38,13 @@ private final class StubbedFeedLoader: FeedLoader {
         stubbedResponse
     }
 
-    static func publishesEmptyResponse() -> AnyPublisher<(articles: [FeedItem], videos: [FeedItem]), Error> {
-        Just(([], [])).mapError{ _ in anyNSError() }.eraseToAnyPublisher()
-    }
-
-    static func publlishesErrorResponse() -> AnyPublisher<(articles: [FeedItem], videos: [FeedItem]), Error> {
+    static func publishesErrorResponse() -> AnyPublisher<(articles: [FeedItem], videos: [FeedItem]), Error> {
         Just(())
             .tryMap { throw anyNSError() }
             .eraseToAnyPublisher()
+    }
+
+    static func publishesSuccessResponse(articles: [FeedItem] = [], videos: [FeedItem] = []) -> AnyPublisher<(articles: [FeedItem], videos: [FeedItem]), Error> {
+        Just((articles, videos)).mapError{ _ in anyNSError() }.eraseToAnyPublisher()
     }
 }
