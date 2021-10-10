83c9905375985914e4098fcf83c199f8203281b0
Andrew Marmion <andrew.marmion@gmail.com>
2021-10-10 17:23:33 +0100

    ViewModel publishes correct lists when response from FeedLoader has only articles, videos and both. It also publishes the correct list when the list changes

--- PracticalExampleTests/ViewModelTests.swift
+++ PracticalExampleTests/ViewModelTests.swift
@@ -31,6 +31,70 @@ final class ViewModelTests: XCTestCase {
         XCTAssertEqual(sut.items, [])
         XCTAssertEqual(sut.selectedList, .all)
     }
+
+    func test_load_successInFeedWithOnlyArticlesResultsCreatesLoadedStateWithArticlesOrdered() {
+
+        let article1 = makeFeedItem(number: 1, type: .article)
+        let article2 = makeFeedItem(number: 2, type: .article)
+
+        let articles = [article2, article1]
+
+        let stubbedResponse = StubbedFeedLoader.publishesSuccessResponse(articles: articles, videos: [])
+        let sut = makeSUT(stubbedResponse: stubbedResponse)
+
+        sut.load()
+
+        XCTAssertEqual(sut.state, .loaded)
+        XCTAssertEqual(sut.items, [article1, article2])
+    }
+
+    func test_load_successInFeedWithOnlyVideosResultsCreatesLoadedStateWithVideosOrdered() {
+        let video1 = makeFeedItem(number: 1, type: .video)
+        let video2 = makeFeedItem(number: 2, type: .video)
+
+        let videos = [video2, video1]
+
+        let stubbedResponse = StubbedFeedLoader.publishesSuccessResponse(articles: [], videos: videos)
+        let sut = makeSUT(stubbedResponse: stubbedResponse)
+
+        sut.load()
+
+        XCTAssertEqual(sut.state, .loaded)
+        XCTAssertEqual(sut.items, [video1, video2])
+    }
+
+    func test_load_successWithBothArticleAndVideoCreatedLoadedStateWithFeedItemsOrderedShowsCorrectListWhenChanged() {
+        let article1 = makeFeedItem(number: 1, type: .article)
+        let article2 = makeFeedItem(number: 2, type: .article)
+
+        let video1 = makeFeedItem(number: 1, type: .video)
+        let video2 = makeFeedItem(number: 2, type: .video)
+
+        let article3 = makeFeedItem(number: 3, type: .article)
+
+        let videos = [video2, video1]
+        let articles = [article2, article1, article3]
+
+        let stubbedResponse = StubbedFeedLoader.publishesSuccessResponse(articles: articles, videos: videos)
+        let sut = makeSUT(stubbedResponse: stubbedResponse)
+
+        sut.load()
+
+        XCTAssertEqual(sut.state, .loaded)
+        XCTAssertEqual(sut.selectedList, .all)
+        XCTAssertEqual(sut.items, [article1, article2, video1, video2, article3])
+
+        sut.selectedList = .articles
+        XCTAssertEqual(sut.items, [article1, article2, article3])
+
+        sut.selectedList = .videos
+        XCTAssertEqual(sut.items, [video1, video2])
+
+        sut.selectedList = .all
+        XCTAssertEqual(sut.items, [article1, article2, video1, video2, article3])
+
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(
@@ -45,6 +109,18 @@ final class ViewModelTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
 
         return sut
+    }
+
+    private func makeFeedItem(number: Int, type: ContentType) -> FeedItem {
+        FeedItem(
+            id: "\(number)",
+            name: "item\(number)",
+            description: "item\(number) description",
+            imageURL: URL(string: "https://item\(number)-url.com")!,
+            releasedDateString: "item\(number) releaseDateString",
+            releasedDate: Date(),
+            itemTypeImage: "item\(number) \(type.rawValue)"
+        )
     }
 
 }
