beeb4e36ed959ea37d3a96d4bf01541b4aefa782
Andrew Marmion <andrew.marmion@gmail.com>
2021-10-10 15:13:59 +0100

    Make FeedItem Equatable

--- Shared/Models/FeedItem.swift
+++ Shared/Models/FeedItem.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct FeedItem: Identifiable {
+public struct FeedItem: Identifiable, Equatable {
     public let id: String
     public let name: String
     public let description: String
