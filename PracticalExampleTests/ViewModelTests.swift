076466a27efece0999f1d8fd5369354c1c846b74
Andrew Marmion <andrew.marmion@gmail.com>
2021-10-10 16:50:19 +0100

    Test initial state of ViewModel

--- PracticalExampleTests/ViewModelTests.swift
+++ PracticalExampleTests/ViewModelTests.swift
@@ -1,11 +1,33 @@
 @testable import PracticalExample
+import Combine
 import XCTest
 
 final class ViewModelTests: XCTestCase {
 
-    func test_init_isLoading() {
-        let sut = ViewModel(client: HTTPClientStub())
+    func test_init_initialState() {
+        let feedLoader = StubbedFeedLoader(stubbedResponse: StubbedFeedLoader.publishesEmptyResponse())
+        let sut = ViewModel(feedLoader: feedLoader)
 
-        XCTAssertEqual(sut.state, .loading)
+        XCTAssertEqual(sut.state, .empty)
+        XCTAssertEqual(sut.items, [])
+        XCTAssertEqual(sut.selectedList, .all)
+    }
+
+}
+
+private final class StubbedFeedLoader: FeedLoader {
+
+    let stubbedResponse: AnyPublisher<(articles: [FeedItem], videos: [FeedItem]), Error>
+
+    init(stubbedResponse: AnyPublisher<(articles: [FeedItem], videos: [FeedItem]), Error>) {
+        self.stubbedResponse = stubbedResponse
+    }
+
+    func load() -> AnyPublisher<(articles: [FeedItem], videos: [FeedItem]), Error> {
+        stubbedResponse
+    }
+
+    static func publishesEmptyResponse() -> AnyPublisher<(articles: [FeedItem], videos: [FeedItem]), Error> {
+        Just(([], [])).mapError{ _ in anyNSError() }.eraseToAnyPublisher()
     }
 }
